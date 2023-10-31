9606b6d17074ba563093ec1fa00adb6542bb1ef0
202144025 이정우 <63216655+wjddn8038@users.noreply.github.com>
2023-10-31 03:28:17 +0000

    Add files via upload

---
+++ dust_sencer.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode, quote_plus, unquote
+import requests
+
+url = 'https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty'
+queryParams = '?' + urlencode({quote_plus('serviceKey') : 't92eTJe4PUS6E8XgI0oLGOO3EGepz/rnwKex5oEKbmUNCcOKAQLOg1LCzFqMbSr3Jr1W4DD2p2yAUgzdhxmCUA=='
+	, quote_plus('returnType') : 'xml'
+	, quote_plus('numOfRows') : '10'
+	, quote_plus('pageNo') : '1'
+	, quote_plus('stationName') : '주안'
+	, quote_plus('dataTerm'):'DAILY'
+	, quote_plus('ver'):'1.0'})
+	
+res = requests.get(url+queryParams)
+soup = BeautifulSoup(res.content, 'html.parser')
+data = soup.find_all('item')
+print(data)
+
+for item in data:
+	datatime = item.find('datatime')
+	pm25value = item.find('pm25value')
+	print(datatime.get_text())
+	print(pm25value.get_text())
+
+import serial
+
+port = '/dev/ttyACM0'
+brate = 9600
+
+seri = serial.Serial(port, baudrate = brate, timeout = None)
+print(seri.name)
+
+seri.write(str.encode('allon'))
+
+a = 1
+while a:
+    if seri.in_waiting !=0 :
+        content = seri.readline()
+        if pm25value.get_text() >= content.decode() :
+            print('open windows')
+        else : 
+            print('no open windows')
+        
+        print(content.decode())
+        a = 0
+
+
